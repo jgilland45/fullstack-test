55851e41eb2c4837061e125be8aca59af1a9af29
Jason Gillanders <jgilland@ualberta.ca>
2026-01-01 16:13:42 -0700

    some cleanups

--- backend/src/main/java/com/example/backend/controller/GreetingController.java
+++ backend/src/main/java/com/example/backend/controller/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import com.example.backend.model.Greeting;
+
 @Controller
 public class GreetingController {
 
@@ -13,7 +15,9 @@ public class GreetingController {
 	@SendTo("/topic/greetings")
 	public Greeting greeting(HelloMessage message) throws Exception {
 		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+		String greetingString = "Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!";
+		System.out.println(greetingString);
+		return new Greeting(greetingString);
 	}
 
 }
